35ec136a0ff73bab9a21d093cdb54f2e094fea60
Ravencodess <115118888+Ravencodess@users.noreply.github.com>
2026-02-10 11:25:26 -0500

    Add account request for alwaysonbiz-rooters (rooters@alwaysonbiz.org)

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,3 +28,32 @@ module "gmail-charlesngerem11-4117" {
 
   account_customizations_name = "sandbox"
 }
+
+module "alwaysonbiz-rooters-ad20" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "rooters@alwaysonbiz.org"
+    AccountName               = "alwaysonbiz-rooters"
+    ManagedOrganizationalUnit = "Sandbox (ou-98w3-u0uez5s0)"
+    SSOUserEmail              = "rooters@alwaysonbiz.org"
+    SSOUserFirstName          = "rooters"
+    SSOUserLastName           = "alwaysonbiz"
+  }
+
+  account_tags = {
+    "Opportunity" = "true"
+    "ManagedBy" = "OpportunityPortal"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Opportunity Portal"
+    change_reason       = "Self-service account creation"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
