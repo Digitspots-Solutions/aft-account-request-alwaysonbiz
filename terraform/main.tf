e2ed0d71f2592b06f13c9ed819d1848e326f6597
Ravencodess <115118888+Ravencodess@users.noreply.github.com>
2026-02-10 10:48:35 -0500

    Add account request for gmail-charlesngerem11 (charlesngerem11@gmail.com)

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,30 @@
 
+
+module "gmail-charlesngerem11-4117" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "charlesngerem11@gmail.com"
+    AccountName               = "gmail-charlesngerem11"
+    ManagedOrganizationalUnit = "Sandbox (ou-98w3-u0uez5s0)"
+    SSOUserEmail              = "charlesngerem11@gmail.com"
+    SSOUserFirstName          = "Charles"
+    SSOUserLastName           = "Ngerem"
+  }
+
+  account_tags = {
+    "Opportunity" = "true"
+    "ManagedBy" = "OpportunityPortal"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Opportunity Portal"
+    change_reason       = "Self-service account creation"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
