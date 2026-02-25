679b8aa415d7fb46fc0abe5b05315ec01358ea06
Ravencodess <115118888+Ravencodess@users.noreply.github.com>
2026-02-25 06:54:49 -0500

    Add account request for thecyberspheretechnologies-info (info@thecyberspheretechnologies.com)

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,3 +28,33 @@ module "alwaysonbiz-rooters-24ac" {
 
   account_customizations_name = "sandbox"
 }
+
+module "thecyberspheretechnologies-info-f181" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "info@thecyberspheretechnologies.com"
+    AccountName               = "thecyberspheretechnologies-info"
+    ManagedOrganizationalUnit = "Website Workloads (ou-98w3-o3cnph0b)"
+    SSOUserEmail              = "info@thecyberspheretechnologies.com"
+    SSOUserFirstName          = "thecyberspheretechnologies"
+    SSOUserLastName           = "thecyberspheretechnologies"
+  }
+
+  account_tags = {
+    "Opportunity" = "true"
+    "ManagedBy" = "OpportunityPortal"
+    "ProvisioningRetry" = "2026-02-25T11:54:48.907485"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Opportunity Portal"
+    change_reason       = "Self-service account creation"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
