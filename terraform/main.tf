9b748b9d411eb7ae3ce09a15aa53fdadb18e808a
Ravencodess <115118888+Ravencodess@users.noreply.github.com>
2026-02-27 09:27:24 +0100

    Add account request for synergyspheretech-admin (admin@synergyspheretech.org)

--- terraform/main.tf
+++ terraform/main.tf
@@ -208,3 +208,33 @@ module "minimalmatrixlabs-com-admin" {
 
   account_customizations_name = "sandbox"
 }
+
+module "synergyspheretech-org-admin" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "admin@synergyspheretech.org"
+    AccountName               = "synergyspheretech-admin"
+    ManagedOrganizationalUnit = "Website Workloads (ou-98w3-o3cnph0b)"
+    SSOUserEmail              = "admin@synergyspheretech.org"
+    SSOUserFirstName          = "synergyspheretech"
+    SSOUserLastName           = "synergyspheretech"
+  }
+
+  account_tags = {
+    "Opportunity" = "true"
+    "ManagedBy" = "OpportunityPortal"
+    "ProvisioningRetry" = "2026-02-27T08:27:24.510086"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Opportunity Portal"
+    change_reason       = "Self-service account creation"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
