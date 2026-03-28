b8f3706a1520646e1af7dd1177f0888364d2b463
Ravencodess <115118888+Ravencodess@users.noreply.github.com>
2026-03-28 02:17:37 +0100

    Add account request for rimzenterprise-admin (admin@rimzenterprise.com)

--- terraform/main.tf
+++ terraform/main.tf
@@ -718,3 +718,33 @@ module "olaiyasuperstore-com-admin" {
 
   account_customizations_name = "sandbox"
 }
+
+module "rimzenterprise-com-admin" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "admin@rimzenterprise.com"
+    AccountName               = "rimzenterprise-admin"
+    ManagedOrganizationalUnit = "Website Workloads (ou-98w3-o3cnph0b)"
+    SSOUserEmail              = "admin@rimzenterprise.com"
+    SSOUserFirstName          = "rimzenterprise"
+    SSOUserLastName           = "rimzenterprise"
+  }
+
+  account_tags = {
+    "Opportunity" = "true"
+    "ManagedBy" = "OpportunityPortal"
+    "ProvisioningRetry" = "2026-03-28T01:17:37.558773"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Opportunity Portal"
+    change_reason       = "Self-service account creation"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
